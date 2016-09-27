dbe01f4945dd4a7a342a9cf87f1f75d6c93008c7
Henrique Marti <henrique@webmarti.com.br>
2016-09-27 17:29:41 -0300

    Melhorias na comunicação

--- LGSmartTV/LGSmartTV.py
+++ LGSmartTV/LGSmartTV.py
@@ -5,13 +5,13 @@ import xml.etree.ElementTree as Etree
 import urllib
 from LGSmartTVQuery import LGSmartTVQuery
 
+header = {"Content-Type": "application/atom+xml", "USER-AGENT": "UDAP/2.0"}
+
 
 class LGSmartTV:
     """
     Classe para interface com SmartTV LG via ethernet
     """
-
-    _header = {"Content-Type": "application/atom+xml"}
 
     @classmethod
     def search(cls):
@@ -19,7 +19,7 @@ class LGSmartTV:
                   'HOST: 239.255.255.250:1900' + '\r\n' + \
                   'MAN: "ssdp:discover"' + '\r\n' + \
                   'MX: 3' + '\r\n' + \
-                  'ST: urn:schemas-udap:service:netrcu:1' + '\r\n' + \
+                  'ST: udap:rootservice' + '\r\n' + \
                   'USER-AGENT: UDAP/2.0' + '\r\n' + '\r\n'
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -32,59 +32,94 @@ class LGSmartTV:
         text_response = urllib.unquote(text_response)
         text_response = text_response.split('\r\n')
 
-        xml = ''
+        url_xml = ''
         for line in text_response:
             if line.split(' ')[0] == 'LOCATION:':
-                xml = line.split(' ')[1]
+                url_xml = line.split(' ')[1]
                 break
 
-        ip = xml.split('://')[1]
+        if url_xml == '':
+            return None
+
+        ip = url_xml.split('://')[1]
         ip = ip.split(':')[0]
 
-        port = xml.split('://')[1]
+        port = url_xml.split('://')[1]
         port = port.split(':')[1]
         port = port.split('/')[0]
 
-        return {'ip': ip, 'port': port, 'xml': xml}
+        url = url_xml.split(port)[1]
+
+        conn = httplib.HTTPConnection(ip, port=port)
+        conn.request('GET', url, '', headers=header)
+
+        tv_lg = False
+
+        xml = Etree.fromstring(conn.getresponse().read())
+        device = xml.find('device')
+        if device:
+            manufacturer = device.find('manufacturer')
+            device_type = device.find('deviceType')
+            if ('LG' in manufacturer.text) and ('TV' in device_type.text):
+                tv_lg = True
+
+        if tv_lg:
+            return {'ip': ip, 'port': port}
+        else:
+            return None
 
     def __init__(self, connection_parameters):
         self.ip = connection_parameters['ip']
         self.port = connection_parameters['port']
-        self.xml = connection_parameters['xml']
         self.pairing_key = 0
         self.paried = False
         self.id_session = 0
         self.query = LGSmartTVQuery(self)
 
-    def pairing(self, pairing_key):
+    def __send_request(self, command):
         conn = httplib.HTTPConnection(self.ip, port=self.port)
-        command = '<?xml version="1.0" encoding="utf-8"?>' + \
-                  '<auth>' + \
-                  '    <type>AuthReq</type>' + \
-                  '    <value>' + str(pairing_key) + '</value>' + \
-                  '</auth>'
-        conn.request('POST', '/roap/api/auth', command, headers=self._header)
+        conn.request('POST', '/udap/api/pairing', command, headers=header)
         http_response = conn.getresponse()
+        return http_response.reason == 'OK'
 
-        root = Etree.fromstring(http_response.read())
+    def pairing_connect(self, pairing_key):
+        """Se conecta com a TV"""
+        command = '<?xml version = "1.0" encoding = "utf-8"?>' + \
+                  '<envelope>' + \
+                  '    <api type = "pairing">' + \
+                  '        <name>hello</name>' + \
+                  '        <value>' + str(pairing_key) + '</value>' + \
+                  '        <port>' + str(self.port) + '</port>' + \
+                  '    </api>' + \
+                  '</envelope>'
 
-        self.id_session = 0
-        for data in root.findall('session'):
-            self.id_session = data.text
-
-        self.paried = http_response.reason == 'OK'
+        self.paried = self.__send_request(command)
 
         if self.paried:
             self.pairing_key = pairing_key
 
         return self.paried
 
+    def pairing_disconnect(self):
+        """Se desconecta da TV"""
+        command = '<?xml version = "1.0" encoding = "utf-8"?>' + \
+                  '<envelope>' + \
+                  '    <api type = "pairing">' + \
+                  '        <name>byebye</name>' + \
+                  '        <port>' + str(self.port) + '</port>' + \
+                  '    </api>' + \
+                  '</envelope>'
+
+        self.paried = not self.__send_request(command)
+        return not self.paried
+
     def display_pairing_key(self):
-        conn = httplib.HTTPConnection(self.ip, port=self.port)
-        command = '<?xml version="1.0" encoding="utf-8"?>' + \
-                  '<auth>' + \
-                  '    <type>AuthKeyReq</type>' + \
-                  '</auth>'
-        conn.request('POST', '/roap/api/auth', command, headers=self._header)
-        http_response = conn.getresponse()
-        return http_response.reason == 'OK'
+        """Mosta a pairing key na tela da TV."""
+        command = '<?xml version = "1.0" encoding = "utf-8"?>' + \
+                  '<envelope>' + \
+                  '    <api type = "pairing">' + \
+                  '        <name>showKey</name>' + \
+                  '    </api>' + \
+                  '</envelope>'
+
+        return self.__send_request(command)
--- LGSmartTV/LGSmartTVQuery.py
+++ LGSmartTV/LGSmartTVQuery.py
@@ -1,101 +1,97 @@
 # coding=utf-8
 import httplib
 import xml.etree.ElementTree as Etree
+import LGSmartTV
 
 
 class LGSmartTVQuery:
-
-    _header = {"Content-Type": "application/atom+xml"}
 
     def __init__(self, lgsmarttv):
         self.LGSmartTV = lgsmarttv
         self.ip = self.LGSmartTV.ip
         self.port = self.LGSmartTV.port
-        # self._header = self.LGSmartTV._header
 
     def __send_request(self, request):
+        if not self.LGSmartTV.paried:
+            raise Exception('Unpaired')
+
         conn = httplib.HTTPConnection(self.ip, port=self.port)
-        conn.request('GET', request, '', headers=self._header)
+        conn.request('GET', request, '', headers=LGSmartTV.header)
         return conn.getresponse().read()
 
     def current_channel(self):
         """Obtem os dados do canal atual em que a TV esta."""
-
-        if not self.LGSmartTV.paried:
-            return None
-
         http_response = self.__send_request('/udap/api/data?target=cur_channel')
 
-        root = Etree.fromstring(http_response)
-
         result = dict()
-        for data in root.findall('data'):
-            result['ch_type'] = data.find('chtype').text
-            result['major'] = data.find('major').text
-            result['display_major'] = data.find('displayMajor').text
-            result['minor'] = data.find('minor').text
-            result['display_minor'] = data.find('displayMinor').text
-            result['source_index'] = data.find('sourceIndex').text
-            result['physical_num'] = data.find('physicalNum').text
-            result['ch_name'] = data.find('chname').text
-            result['prog_name'] = data.find('progName').text
-            result['audio_ch'] = data.find('audioCh').text
-            result['input_source_name'] = data.find('inputSourceName').text
-            result['input_source_type'] = data.find('inputSourceType').text
-            result['input_source_index'] = data.find('inputSourceIdx').text
-            result['label_name'] = data.find('labelName').text
+
+        root = Etree.fromstring(http_response)
+        data_list = root.find('dataList')
+        if data_list:
+            if ('name' in data_list.attrib) and (data_list.attrib['name'] == 'Current Channel Info'):
+                data = data_list.find('data')
+                if data:
+                    result['ch_type'] = data.find('chtype').text
+                    result['major'] = data.find('major').text
+                    result['display_major'] = data.find('displayMajor').text
+                    result['minor'] = data.find('minor').text
+                    result['display_minor'] = data.find('displayMinor').text
+                    result['source_index'] = data.find('sourceIndex').text
+                    result['physical_num'] = data.find('physicalNum').text
+                    result['ch_name'] = data.find('chname').text
+                    result['prog_name'] = data.find('progName').text
+                    result['audio_ch'] = data.find('audioCh').text
+                    result['input_source_name'] = data.find('inputSourceName').text
+                    result['input_source_type'] = data.find('inputSourceType').text
+                    result['input_source_index'] = data.find('inputSourceIdx').text
+                    result['label_name'] = data.find('labelName').text
 
         return result
 
     def channel_list(self):
         """Obtem os dados do canal atual em que a TV esta."""
-
-        if not self.LGSmartTV.paried:
-            return None
-
         http_response = self.__send_request('/udap/api/data?target=channel_list')
 
-        root = Etree.fromstring(http_response)
-
         result = list()
-        for data in root.findall('data'):
-            channel = dict()
-            channel['ch_type'] = data.find('chtype').text
-            channel['major'] = data.find('major').text
-            channel['minor'] = data.find('minor').text
-            channel['source_index'] = data.find('sourceIndex').text
-            channel['physical_num'] = data.find('physicalNum').text
-            channel['ch_name'] = data.find('chname').text
 
-            result.append(channel)
+        root = Etree.fromstring(http_response)
+        data_list = root.find('dataList')
+        if data_list:
+            if ('name' in data_list.attrib) and (data_list.attrib['name'] == 'Channel List'):
+                for data in data_list.findall('data'):
+                    channel = dict()
+                    channel['ch_type'] = data.find('chtype').text
+                    channel['major'] = data.find('major').text
+                    channel['minor'] = data.find('minor').text
+                    channel['source_index'] = data.find('sourceIndex').text
+                    channel['physical_num'] = data.find('physicalNum').text
+                    channel['ch_name'] = data.find('chname').text
+
+                    result.append(channel)
 
         return result
 
     def volume(self):
         """Obtem os dados do volume em que a TV esta."""
-
-        if not self.LGSmartTV.paried:
-            return None
-
         http_response = self.__send_request('/udap/api/data?target=volume_info')
 
-        root = Etree.fromstring(http_response)
-
         result = dict()
-        for data in root.findall('data'):
-            result['mute'] = data.find('mute').text
-            result['min_level'] = data.find('minLevel').text
-            result['max_level'] = data.find('maxLevel').text
-            result['level'] = data.find('level').text
+
+        root = Etree.fromstring(http_response)
+        data_list = root.find('dataList')
+        if data_list:
+            if ('name' in data_list.attrib) and (data_list.attrib['name'] == 'Volume Info'):
+                data = data_list.find('data')
+                if data:
+                    result['mute'] = data.find('mute').text
+                    result['min_level'] = data.find('minLevel').text
+                    result['max_level'] = data.find('maxLevel').text
+                    result['level'] = data.find('level').text
 
         return result
 
     def screen_capture(self, file_name):
         """Captura a imagem da tela da TV e salva no arquivo indicado."""
-
-        if not self.LGSmartTV.paried:
-            return None
-
         http_response = self.__send_request('/udap/api/data?target=screen_image')
 
         f = open(file_name, 'wb')
@@ -103,56 +99,47 @@ class LGSmartTVQuery:
 
     def operation_mode(self):
         """Obtem os dados do volume em que a TV esta."""
-
-        if not self.LGSmartTV.paried:
-            return None
-
         http_response = self.__send_request('/udap/api/data?target=context_ui')
 
-        root = Etree.fromstring(http_response)
-
         result = dict()
-        for data in root.findall('data'):
-            if data.find('mode').text == 'VolCh':
-                result['mode'] = 0
-            else:
-                result['mode'] = 1
 
-            result['mode_string'] = data.find('mode').text
-
+        root = Etree.fromstring(http_response)
+        data_list = root.find('dataList')
+        if data_list:
+            if ('name' in data_list.attrib) and (data_list.attrib['name'] == 'TV UI Mode'):
+                data = data_list.find('data')
+                if data:
+                    result['mode_string'] = data.find('mode').text
+                    if result['mode_string'] == 'VolCh':
+                        result['mode'] = 0
+                    else:
+                        result['mode'] = 1
         return result
 
     def app_list(self):
         """Obtem a lista de Apps instalados na TV."""
-
-        if not self.LGSmartTV.paried:
-            return None
-
         http_response = self.__send_request('/udap/api/data?target=applist_get&type=1&index=1&number=1024')
-
-        root = Etree.fromstring(http_response)
 
         result = list()
 
-        for data in root.findall('data'):
-            app = dict()
-            app['auid'] = data.find('auid').text
-            app['name'] = data.find('name').text
-            app['type'] = data.find('type').text
-            app['cpid'] = data.find('cpid').text
-            app['adult'] = data.find('adult').text
-            app['icon_name'] = data.find('icon_name').text
+        root = Etree.fromstring(http_response)
+        data_list = root.find('dataList')
+        if data_list:
+            if ('name' in data_list.attrib) and (data_list.attrib['name'] == 'Max App List'):
+                for data in data_list.findall('data'):
+                    app = dict()
+                    app['auid'] = data.find('auid').text
+                    app['name'] = data.find('name').text
+                    app['type'] = data.find('type').text
+                    app['cpid'] = data.find('cpid').text
+                    app['adult'] = data.find('adult').text
+                    app['icon_name'] = data.find('icon_name').text
 
-            result.append(app)
-
+                    result.append(app)
         return result
 
     def app_icon(self, app_auid, file_name):
         """Salva a icone do App no arquivo indicado. (O arquivo tem que ser PNG)"""
-
-        if not self.LGSmartTV.paried:
-            return None
-
         http_response = self.__send_request('/udap/api/data?target=appicon_get&auid=' + app_auid
                                             + '&appname=URL_Encode()')
 
@@ -164,29 +151,25 @@ class LGSmartTVQuery:
         Type: 1 = All apps
               2 = Premium
               3 = My Apps"""
-
-        if not self.LGSmartTV.paried:
-            return None
-
         http_response = self.__send_request('/udap/api/data?target=appnum_get&type=' + str(app_type))
 
+        result = dict()
+
         root = Etree.fromstring(http_response)
+        data_list = root.find('dataList')
+        if data_list:
+            if ('name' in data_list.attrib) and (data_list.attrib['name'] == 'App Num'):
+                data = data_list.find('data')
+                if data:
+                    if data.find('type').text == '1':
+                        result['type'] = 'All'
 
-        result = list()
-        for data in root.findall('data'):
-            count = dict()
+                    if data.find('type').text == '2':
+                        result['type'] = 'My Apps'
 
-            if data.find('type').text == '1':
-                count['type'] = 'All'
+                    if data.find('type').text == '3':
+                        result['type'] = 'Premium'
 
-            if data.find('type').text == '2':
-                count['type'] = 'My Apps'
-
-            if data.find('type').text == '3':
-                count['type'] = 'Premium'
-
-            count['count'] = data.find('number').text
-
-            result.append(count)
+                    result['count'] = data.find('number').text
 
         return result
